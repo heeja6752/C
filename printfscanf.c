d267952ff7c2a07a566c8c5e0c50196652995397
heeja6752 <43363703+heeja6752@users.noreply.github.com>
2021-06-11 18:04:15 +0900

    Update printfscanf.c

--- printfscanf.c
+++ printfscanf.c
@@ -22,5 +22,34 @@ int main(void)
 	//print("태어난 년도 : %d\n", Year);
 	//Year = 2001;
 
+	//printf
+	//연산
+	//int add = 3 + 7; //10
+	//printf("3 + 7 = %d\n", add);
+	//printf("%d + %d = %d\n", 30, 79, 30 * 79);
+
+	//scanf
+	//키보드 입력을 받아서 저장
+
+	//int input;
+	//printf("값을 입력하세요 : ");
+	//scanf_s("%d", &input);
+	//printf("입력값 : %d\n", input);
+
+	/*int one, two, three;
+	printf("3개의 정수를 입력하세요 : ");
+	scanf_s("%d %d %d", &one, &two, &three);
+	printf("첫번쨰 값 : %d\n", one);
+	printf("두번째 값 : %d\n", two);
+	printf("세번쨰 값 : %d\n", three);*/
+
+	//문자(한 글자), 문자열(한 글자 이상의 여러 글자)
+	//char c = 'A';
+	//printf("c\n", c);
+
+	char str[256];
+	scanf_s("%s", str, sizeof(str));
+	printf("%s\n", str);
+
 	return 0;
 }

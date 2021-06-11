5961332cc1473ef3cbb8774681b7f49bdaa75ba2
heeja6752 <43363703+heeja6752@users.noreply.github.com>
2021-06-11 17:32:55 +0900

    Update printfscanf.c

--- printfscanf.c
+++ printfscanf.c
@@ -8,8 +8,19 @@ int main(void)
 	printf("%d\n", age);
 	*/
 
-	printf("1\n");
+	//printf("1\n");
 	//printf("2\n"); //2는 범위에서 벗어나므로 주석처리
-	printf("3\n");
+	//printf("3\n");
+	
+	//실수형 변수에 대한 예제
+	//float f = 46.5;
+	//printf("%.2f\n", f);
+	//double d = 4.428;
+	//printf("%.2f\n", d);
+	
+	//const int Year = 2000; //상수
+	//print("태어난 년도 : %d\n", Year);
+	//Year = 2001;
+
 	return 0;
 }
